c85d113bdd71157aef71c0410eb166e5c96cb136
Ibrahim mohamed <ebrahimaboeita990@gmail.com>
2022-05-14 14:15:54 +0200

    Initial app

---
+++ App/main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+class MainWidget(Widget):
+    pass
+
+
+class MedicalApp(App):
+    pass
+
+
+MedicalApp().run()
